b33af34bfa0659ffd150e627cd77353fc45d8d84
kazumatu981 <kazumatu981@gmail.com>
2025-03-07 08:22:23 +0900

    add: 抽象クラス `PaseTreeNode` の追加

---
+++ src/parser/pase-tree-nodes/parse-tree-node.ts
@@ -0,0 +1,52 @@
+import { type Token } from '../../tokenizer';
+
+export type NodeType = 'single' | 'binary' | 'paren';
+export type StringifyType = 'thisNode' | 'resolving' | 'resolved';
+
+export abstract class PaseTreeNode {
+    private readonly _type: NodeType;
+    private readonly _value: Token[];
+    private _parent?: PaseTreeNode;
+
+    /**
+     * @param type ノードの型
+     * @param tokens 使われた字句の配列
+     */
+    constructor(type: NodeType, tokens: Token[]) {
+        this._type = type;
+        this._value = tokens;
+    }
+
+    /**
+     * ノードの型を取得します
+     * @returns ノードの型
+     */
+    public get nodeType(): NodeType {
+        return this._type;
+    }
+
+    /**
+     * この ParseTreeNodes の値を取得します
+     * @returns この ParseTreeNodes の値
+     */
+    public get value(): Token[] {
+        return this._value;
+    }
+
+    /**
+     * この ParseTreeNodes の親 ParseTreeNodes を取得します
+     * @returns 親 ParseTreeNodes
+     */
+    public get parent(): PaseTreeNode | undefined {
+        return this._parent;
+    }
+    /**
+     * この ParseTreeNodes の親 ParseTreeNodes を設定します
+     * @param value 設定する親 ParseTreeNodes
+     */
+    protected set parent(value: PaseTreeNode | undefined) {
+        this._parent = value;
+    }
+
+    public abstract toString(type: StringifyType): string;
+}
