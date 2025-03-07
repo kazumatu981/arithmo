4d5792189c9e4b1b50874e9aec3976e458fa4922
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2025-03-07 18:44:46 +0900

    just backup.

--- src/parser/parse-tree-builder.ts
+++ src/parser/parse-tree-builder.ts
@@ -2,7 +2,7 @@ import { Token } from '../tokenizer';
 import { PaseTreeNode } from './pase-tree-nodes';
 
 type ParseState = 'Initial' | 'WaitForNumber' | 'WaitForOperator';
-type StateAction: (token: Token) => void;
+type StateAction = (token: Token) => void;
 export class ParseTreeBuilder {
     private _currentNode?: PaseTreeNode;
     private state: ParseState = 'Initial';
@@ -11,25 +11,51 @@ export class ParseTreeBuilder {
         Initial: this.onInitial,
         WaitForNumber: this.onWaitForNumber,
         WaitForOperator: this.onWaitForOperator,
-    }
+    };
     public addToken(token: Token): this {
         const action = this.stateActions[this.state];
         action(token);
         return this;
     }
-    private onInitial(token: Token) {
+    private onInitial(token: Token): void {
         if (token.isNumber) {
             this.state = 'WaitForOperator';
-        } if( token.isNegativeSign ) {
+            this.appendNumberNode(token);
+        }
+        if (token.isNegativeSign) {
             this.state = 'WaitForNumber';
         } else {
-            // todo error
+            // TODO error
         }
     }
-    private onWaitForNumber(token: Token) {}
-    private onWaitForOperator(token: Token) {}
+    private onWaitForNumber(token: Token): void {
+        if (token.isNumber) {
+            this.state = 'WaitForOperator';
+            this.appendNumberNode(token);
+        }
+        if (token.isLeftParen) {
+            this.state = 'Initial';
+            this.appendParenStart(token);
+        } else {
+            // TODO error
+        }
+    }
+    private onWaitForOperator(token: Token): void {
+        if (token.isOperator) {
+            this.state = 'WaitForNumber';
+            this.appendOperatorNode(token);
+        } else if (token.isRightParen) {
+            this.appendParenEnd(token);
+            this.state = 'WaitForOperator';
+        } else {
+            // TODO error
+        }
+    }
 
-    private appendNumberNode(token: Token) {}
+    private appendNumberNode(token: Token) {
+        const number;
+    }
     private appendOperatorNode(token: Token) {}
-    private appendParenNode(token: Token) {}
+    private appendParenStart(token: Token) {}
+    private appendParenEnd(token: Token) {}
 }
