f9cd28fa0ed25f839e310407432195f09d1a0afd
kazumatu981 <kazumatu981@gmail.com>
2025-03-16 21:24:55 +0900

    refactor and add TSDoc.

--- src/parser/parse-tree-builder.ts
+++ src/parser/parse-tree-builder.ts
@@ -9,101 +9,164 @@ import type { ErrorCode } from '../common/error-messages';
 import { ParserError } from './parser-error';
 import { UnexpectedError } from '../common/unexpected-error';
 
-type ParseState = 'Initial' | 'WaitForNumber' | 'WaitForOperator';
-type ParseTreeStateTableRecord<T> = Record<TokenType, T>;
-type ParseTreeStateTable<T> = Record<ParseState, ParseTreeStateTableRecord<T>>;
+/**
+ * 構文解析木の状態
+ */
+type ParseState = 'initial' | 'waitForNumber' | 'waitForOperator';
+/**
+ * 構文解析木の状態に応じたアクション
+ */
 type StateAction = (token: Token) => void;
 
-export class ParseTreeBuilder {
-    private _currentNode?: ParseTreeNode;
-    private state: ParseState = 'Initial';
-    private sign: Token | null = null;
+/**
+ * 状態に応じた決定テーブルレコード(行)
+ */
+type ParseTreeStateTableRecord<T> = Record<TokenType, T>;
+/**
+ * 状態に応じた決定テーブル
+ */
+type ParseTreeStateTable<T> = Record<ParseState, ParseTreeStateTableRecord<T>>;
 
-    private nextStateTable: ParseTreeStateTable<ParseState | undefined> = {
-        Initial: {
-            number: 'WaitForOperator',
-            leftParen: 'Initial',
+/**
+ * 状態遷移テーブル
+ */
+type StateTable = ParseTreeStateTable<ParseState | undefined>;
+/**
+ * アクションテーブル
+ */
+type ActionTable = ParseTreeStateTable<StateAction>;
+
+/**
+ * 構文解析木を構築するクラス
+ */
+export class ParseTreeBuilder {
+    // #region private fields
+    /**
+     * 現在のノード
+     */
+    private _currentNode?: ParseTreeNode;
+    /**
+     * 現在の状態
+     */
+    private _state: ParseState = 'initial';
+    /**
+     * 符号のキャッシュ
+     */
+    private _sign: Token | null = null;
+    /**
+     * 状態遷移テーブル
+     */
+    private readonly _stateTable: StateTable = {
+        initial: {
+            number: 'waitForOperator',
+            leftParen: 'initial',
             rightParen: undefined,
-            operator: 'WaitForNumber',
+            operator: 'waitForNumber',
         },
-        WaitForNumber: {
-            number: 'WaitForOperator',
-            leftParen: 'Initial',
+        waitForNumber: {
+            number: 'waitForOperator',
+            leftParen: 'initial',
             rightParen: undefined,
             operator: undefined,
         },
-        WaitForOperator: {
+        waitForOperator: {
             number: undefined,
             leftParen: undefined,
-            rightParen: 'WaitForOperator',
-            operator: 'WaitForNumber',
+            rightParen: 'waitForOperator',
+            operator: 'waitForNumber',
         },
     };
-
-    private stateActionTable: ParseTreeStateTable<StateAction | undefined> = {
-        Initial: {
-            number: this.appendNumberNode.bind(this),
-            leftParen: this.appendParenStart.bind(this),
+    /**
+     * アクションテーブル
+     */
+    private readonly _actionTable: ActionTable = {
+        initial: {
+            number: this._appendNumberNode.bind(this),
+            leftParen: this._appendParenStart.bind(this),
             rightParen: ((token: Token): void => {
-                this.throwParseError('unexpected-right-paren', token);
+                this._throwParseError('unexpected-right-paren', token);
             }).bind(this),
-            operator: this.appendSign.bind(this),
+            operator: this._appendSign.bind(this),
         },
-        WaitForNumber: {
-            number: this.appendNumberNode.bind(this),
-            leftParen: this.appendParenStart.bind(this),
+        waitForNumber: {
+            number: this._appendNumberNode.bind(this),
+            leftParen: this._appendParenStart.bind(this),
             rightParen: ((token: Token): void => {
-                this.throwParseError('unexpected-right-paren', token);
+                this._throwParseError('unexpected-right-paren', token);
             }).bind(this),
             operator: ((token: Token): void => {
-                this.throwParseError('unexpected-operator', token);
+                this._throwParseError('unexpected-operator', token);
             }).bind(this),
         },
-        WaitForOperator: {
+        waitForOperator: {
             number: ((token: Token): void => {
-                this.throwParseError('unexpected-number', token);
+                this._throwParseError('unexpected-number', token);
             }).bind(this),
             leftParen: ((token: Token): void => {
-                this.throwParseError('unexpected-left-paren', token);
+                this._throwParseError('unexpected-left-paren', token);
             }).bind(this),
-            rightParen: this.appendParenEnd.bind(this),
-            operator: this.appendOperatorNode.bind(this),
+            rightParen: this._appendParenEnd.bind(this),
+            operator: this._appendOperatorNode.bind(this),
         },
     };
+    // #endregion
+
+    /**
+     * 構文解析木を初期化する
+     */
+    public initialize(): void {
+        this._currentNode = undefined;
+        this._state = 'initial';
+        this._sign = null;
+    }
+
+    /**
+     * 構文木にトークンを追加する
+     * @param token - 追加するトークン
+     * @returns 構文解析木
+     */
     public addToken(token: Token): this {
-        const action = this.stateActionTable[this.state][token.type];
-        const nextState = this.nextStateTable[this.state][token.type];
+        // テーブル上のアクションを実行
+        this._actionTable[this._state][token.type](token);
+
+        // 次の状態へ移行する
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        action!(token);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.state = nextState!;
+        this._state = this._stateTable[this._state][token.type]!;
+
         return this;
     }
+
+    /**
+     * 構文木のルートノードを返却する
+     * @returns ルートノード
+     */
     public findRootNode(): ParseTreeNode | undefined {
         return ParseTreeNode.findRootNode(this._currentNode);
     }
+
+    /**
+     * ツリーを検証して構文解析木を返却する
+     * @returns 構文解析木
+     */
     public build(): ParseTreeNode | undefined {
         const rootNode = this.findRootNode();
         rootNode?.test();
         return rootNode;
     }
 
-    private throwParseError(code: ErrorCode, token: Token): void {
-        throw new ParserError(code, { token });
-    }
-    private appendSign(token: Token): void {
+    // #region private methods
+    private _appendSign(token: Token): void {
         if (token.isNegativeSign) {
-            this.sign = token;
+            this._sign = token;
         } else {
             throw new ParserError('unexpected-sign', { token });
         }
     }
-
-    private appendNumberNode(token: Token): void {
-        const numberNode = this.sign
-            ? new SingleNode([this.sign, token])
+    private _appendNumberNode(token: Token): void {
+        const numberNode = this._sign
+            ? new SingleNode([this._sign, token])
             : new SingleNode([token]);
-        this.sign = null;
+        this._sign = null;
         if (this._currentNode) {
             if (this._currentNode.nodeType === 'binary') {
                 (this._currentNode as BinaryNode).right = numberNode;
@@ -118,14 +181,14 @@ export class ParseTreeBuilder {
         }
         this._currentNode = numberNode;
     }
-    private appendOperatorNode(token: Token): void {
+    private _appendOperatorNode(token: Token): void {
         this._currentNode = new BinaryNode([token]).attachTo(this._currentNode);
     }
-    private appendParenStart(token: Token): void {
-        const parenNode = this.sign
-            ? new ParenNode([this.sign, token])
+    private _appendParenStart(token: Token): void {
+        const parenNode = this._sign
+            ? new ParenNode([this._sign, token])
             : new ParenNode([token]);
-        this.sign = null;
+        this._sign = null;
         if (this._currentNode) {
             if (this._currentNode.nodeType === 'binary') {
                 (this._currentNode as BinaryNode).right = parenNode;
@@ -140,7 +203,7 @@ export class ParseTreeBuilder {
         }
         this._currentNode = parenNode;
     }
-    private appendParenEnd(token: Token): void {
+    private _appendParenEnd(token: Token): void {
         const parenNode = ParenNode.findParenNode(this._currentNode);
         if (!parenNode) {
             throw new ParserError('unexpected-right-paren', { token });
@@ -148,4 +211,8 @@ export class ParseTreeBuilder {
         parenNode.parenEnd = token;
         this._currentNode = parenNode;
     }
+    private _throwParseError(code: ErrorCode, token: Token): void {
+        throw new ParserError(code, { token });
+    }
+    // #endregion
 }
