148ac48c0c9ead8b56860680aaba0e17f7517a4a
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2025-03-23 20:04:55 +0900

    added numeric resolver

---
+++ __tests__/unit/resolver/numeric-resolver.test.ts
@@ -0,0 +1,23 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { tokenize } from '../../../src/tokenizer';
+import { parse } from '../../../src/parser';
+import { NumericResolver } from '../../../src/resolver';
+
+describe('NumericResolver は整数しか扱いません', () => {
+    test('1+1', () => {
+        const tokens = tokenize('1+1');
+        const nodes = parse(tokens);
+        const resolver = new NumericResolver();
+        expect(() => {
+            resolver.resolve(nodes!);
+        }).not.toThrow();
+    });
+    test('1/2', () => {
+        const tokens = tokenize('1/2');
+        const nodes = parse(tokens);
+        const resolver = new NumericResolver();
+        expect(() => {
+            resolver.resolve(nodes!);
+        }).toThrow();
+    });
+});
