723c3534e48add996f5fc432fa55582097670c52
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2025-04-12 12:07:41 +0900

    implemented.

--- src/resolver/rational-number-resolver/rational-base.ts
+++ src/resolver/rational-number-resolver/rational-base.ts
@@ -1,40 +1,66 @@
 import type { Ring, Field } from '../common/arithmetic-operations';
 
-export abstract class RationalBase<TThis, TBase extends Ring<TBase>>
-    implements Field<TThis>
+export abstract class RationalBase<
+    TThis extends RationalBase<TThis, TBase>,
+    TBase extends Ring<TBase>,
+> implements Field<TThis>
 {
-    abstract readonly _constructable: {
+    abstract readonly constructable: {
         new (numerator: TBase, denominator: TBase): TThis;
     };
-    public abstract denominator: TBase;
+    /**
+     * 分子
+     */
     public abstract numerator: TBase;
-    public abstract isNegative: boolean;
+    /**
+     * 分母
+     */
+    public abstract denominator: TBase;
 
-    public add(_b: TThis): TThis {
-        throw new Error('not implemented');
+    /**
+     * 足し算
+     * @param other 足す数
+     * @returns 計算結果
+     */
+    public add(other: TThis): TThis {
+        const firstElement = this.numerator.multiply(other.denominator);
+        const secondElement = this.denominator.multiply(other.numerator);
+
+        const newNumerator = firstElement.add(secondElement);
+        const newDenominator = this.denominator.multiply(other.denominator);
+        return new this.constructable(newNumerator, newDenominator);
     }
-    public multiply(_b: TThis): TThis {
-        throw new Error('not implemented');
+
+    public multiply(other: TThis): TThis {
+        const newNumerator = this.numerator.multiply(other.numerator);
+        const newDenominator = this.denominator.multiply(other.denominator);
+        return new this.constructable(newNumerator, newDenominator);
     }
     public negate(): TThis {
-        throw new Error('not implemented');
+        return new this.constructable(
+            this.numerator.negate(),
+            this.denominator,
+        );
     }
+
     public reciprocate(): TThis {
-        throw new Error('not implemented');
+        return new this.constructable(this.denominator, this.numerator);
     }
     public zero(): TThis {
-        return new this._constructable(
+        return new this.constructable(
             this.numerator.zero(),
             this.denominator.unit(),
         );
     }
     public unit(): TThis {
-        return new this._constructable(
+        return new this.constructable(
             this.numerator.unit(),
             this.denominator.unit(),
         );
     }
-    public equals(_b: TThis): boolean {
-        throw new Error('not implemented');
+    public equals(other: TThis): boolean {
+        return this.numerator
+            .multiply(other.denominator)
+            .equals(this.denominator.multiply(other.numerator));
     }
 }
--- src/resolver/rational-number-resolver/rational-number.ts
+++ src/resolver/rational-number-resolver/rational-number.ts
@@ -1,165 +1,27 @@
-import { gcd, isNumeric } from '../common/numeric';
-import { Testable, type Rule } from '../../common/testable';
-import { Field } from '../common/arithmetic-operations';
+import { Numeric } from '../numeric-resolver/numeric';
+import { RationalBase } from './rational-base';
 
-export class RationalNumber
-    extends Testable<RationalNumber>
-    implements Field<RationalNumber>
-{
+export class RationalNumber extends RationalBase<RationalNumber, Numeric> {
+    readonly constructable = RationalNumber;
     /**
      * 分子
      */
-    private _numerator: number = 0;
+    public numerator: Numeric;
     /**
      * 分母
      */
-    private _denominator: number = 1;
-    private _isNegative: boolean = false;
+    public denominator: Numeric;
 
-    constructor(
-        numerator: number,
-        denominator: number,
-        isNegative: boolean = false,
+    public constructor(
+        numerator: Numeric | number,
+        denominator: Numeric | number,
     ) {
         super();
-        this._numerator = numerator;
-        this._denominator = denominator;
-        this._isNegative = isNegative;
-        this.test();
+        this.numerator =
+            typeof numerator === 'number' ? new Numeric(numerator) : numerator;
+        this.denominator =
+            typeof denominator === 'number'
+                ? new Numeric(denominator)
+                : denominator;
     }
-    public get numerator(): number {
-        return this._numerator;
-    }
-
-    public set numerator(value: number) {
-        this._numerator = value;
-        this.test();
-    }
-    public get denominator(): number {
-        return this._denominator;
-    }
-    public set denominator(value: number) {
-        this._denominator = value;
-        this.test();
-    }
-    public get isNegative(): boolean {
-        return this._isNegative;
-    }
-    public set isNegative(value: boolean) {
-        this._isNegative = value;
-    }
-
-    public add(other: RationalNumber): RationalNumber {
-        const numerator =
-            (this.isNegative ? -1 : 1) * this.numerator * other.denominator +
-            (other.isNegative ? -1 : 1) * this.denominator * other.numerator;
-        const denominator = this.denominator * other.denominator;
-
-        return new RationalNumber(
-            numerator < 0 ? -numerator : numerator,
-            denominator,
-            numerator < 0,
-        );
-    }
-
-    public subtract(other: RationalNumber): RationalNumber {
-        const negated = other.negate();
-        return this.add(negated);
-    }
-    public multiply(other: RationalNumber): RationalNumber {
-        const numerator = this.numerator * other.numerator;
-        const denominator = this.denominator * other.denominator;
-        return new RationalNumber(
-            numerator,
-            denominator,
-            this.isNegative !== other.isNegative,
-        );
-    }
-
-    public divide(other: RationalNumber): RationalNumber {
-        const reciprocated = other.reciprocate();
-        return this.multiply(reciprocated);
-    }
-
-    public negate(): RationalNumber {
-        return new RationalNumber(
-            this.numerator,
-            this.denominator,
-            !this.isNegative,
-        );
-    }
-
-    public reciprocate(): RationalNumber {
-        return new RationalNumber(
-            this.denominator,
-            this.numerator,
-            this.isNegative,
-        );
-    }
-
-    public zero(): RationalNumber {
-        return Zero;
-    }
-
-    public unit(): RationalNumber {
-        return One;
-    }
-
-    public equals(other: RationalNumber): boolean {
-        return (
-            this.numerator * other.denominator ===
-                this.denominator * other.numerator &&
-            this.isNegative === other.isNegative
-        );
-    }
-    public toString(): string {
-        const signature = this.isNegative ? '-' : '';
-        return this.numerator === 0
-            ? '0'
-            : `${signature}${this.numerator}/${this.denominator}`;
-    }
-    protected normalize(): void {
-        const gcdOfNumeratorAndDenominator = gcd(
-            this.numerator,
-            this.denominator,
-        );
-        this._numerator /= gcdOfNumeratorAndDenominator;
-        this._denominator /= gcdOfNumeratorAndDenominator;
-    }
-    protected readonly moduleName = 'resolver';
-    protected readonly rules: Rule<RationalNumber>[] = [
-        (r): void => {
-            const rationalNumber = r as RationalNumber;
-            if (rationalNumber.denominator === 0) {
-                throw new Error('分母が0です');
-            }
-        },
-        (r): void => {
-            const rationalNumber = r as RationalNumber;
-            if (rationalNumber.denominator < 0) {
-                throw new Error('分母が負の数です');
-            }
-        },
-        (r): void => {
-            const rationalNumber = r as RationalNumber;
-            if (rationalNumber.numerator < 0) {
-                throw new Error('分子が負の数です');
-            }
-        },
-        (r): void => {
-            const rationalNumber = r as RationalNumber;
-            if (!isNumeric(rationalNumber.numerator)) {
-                throw new Error('分子が小数です');
-            }
-        },
-        (r): void => {
-            const rationalNumber = r as RationalNumber;
-            if (!isNumeric(rationalNumber.denominator)) {
-                throw new Error('分母が小数です');
-            }
-        },
-    ];
 }
-
-export const Zero: RationalNumber = new RationalNumber(0, 1);
-export const One: RationalNumber = new RationalNumber(1, 1);
